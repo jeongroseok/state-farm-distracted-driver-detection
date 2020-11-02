6736869b07d71947e62359417ecf44cee01c8040
jeongroseok <jeongroseok@gmail.com>
2020-11-03 01:02:22 +0900

    임시 커밋

--- src/models/vgg.py
+++ src/models/vgg.py
@@ -8,6 +8,9 @@ class VGG11(nn.Module):
         num_classes: int = 1000,
     ) -> None:
         super(VGG11, self).__init__()
+        """
+        특징 추출 레이어
+        """
         self.features = nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
@@ -31,7 +34,17 @@ class VGG11(nn.Module):
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
         )
+        """
+        Conv + Fully Conn 구성 대비,
+        Conv + AdaptiveAvgPooling + Fully Connected 구성이 다음과 같은 장점을 가짐:
+        1. 다양한 입력 이미지 사이즈
+        2. 학습 파라미터 수 감소
+        """
         self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
+        """
+        앞선 conv layer에서 특징을 추출했다면,
+        추출된 특징들(512개)을 이용해 분류
+        """
         self.classifier = nn.Sequential(
             nn.Linear(512 * 7 * 7, 4096),
             nn.ReLU(True),
---
+++ src/utils/distracted_driver_label.py
@@ -0,0 +1,12 @@
+label_dict = {
+    0: "safe driving (안전운전 중)",
+    1: "texting - right (문자메시지 중 - 오른쪽)",
+    2: "talking on the phone - right (전화로 대화 중 - 오른쪽)",
+    3: "texting - left (문자메시지 중 - 왼쪽)",
+    4: "talking on the phone - left (전화로 대화 중 - 왼쪽)",
+    5: "operating the radio (라디오 조작 중)",
+    6: "drinking (음료수 마시기)",
+    7: "reaching behind (뒤쪽에 손뻗기)",
+    8: "hair and makeup (머리와 화장 고치기)",
+    9: "talking to passenger (동승자와 대화)"
+}
